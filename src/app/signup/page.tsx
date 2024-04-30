84f3b6604a2930c7a6a4ac595d4c95cc51c1cc35
Parchello <parchellos@gmail.com>
2024-04-30 20:31:20 +0300

    profile and id page add

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,11 +15,11 @@ export default function SignUpPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Login</h1>
+      <h1>Register Page</h1>
       <hr />
       <label htmlFor="username">Username</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         id="username"
         type="text"
         value={user.username}
@@ -28,7 +28,7 @@ export default function SignUpPage() {
       />
       <label htmlFor="email">Email</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         id="email"
         type="text"
         value={user.email}
@@ -37,13 +37,22 @@ export default function SignUpPage() {
       />
       <label htmlFor="password">Password</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         id="password"
         type="text"
         value={user.password}
         onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="password"
       />
+      <button
+        onClick={onSignup}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Sign Up
+      </button>
+      <Link className="hover:bg-green-500" href="/login">
+        Login Page
+      </Link>
     </div>
   );
 }

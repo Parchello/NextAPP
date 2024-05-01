5f5304601120924911b21d450271d4647d1e9ce3
Parchello <parchellos@gmail.com>
2024-05-01 13:23:37 +0300

    signup route add

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,11 +5,13 @@ import { useRouter } from "next/navigation";
 import { axios } from "axios";
 
 export default function SignUpPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   });
+  const [buttonDisavled, setButtonDisavled] = React.useState(true);
 
   const onSignup = async () => {};
 

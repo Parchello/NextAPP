a231d8bc1c54902b8ed4a2dbff885bac70ecb3b6
Parchello <parchellos@gmail.com>
2024-05-01 12:47:35 +0300

    user model add

---
+++ src/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    unique: true,
+  },
+});
+
+const User = mongoose.models.users || mongoose.model("User", userSchema);
+
+export default User;

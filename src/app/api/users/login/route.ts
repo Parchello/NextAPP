f0c3c841931e63bc3e18c397a76bd25917256ddc
Parchello <parchellos@gmail.com>
2024-05-02 14:16:56 +0300

    login route add and bcrypt types

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,33 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { error } from "console";
+
+connect();
+
+export async function Post(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
+    }
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        { error: "Password is incorrect" },
+        { status: 400 }
+      );
+    }
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}

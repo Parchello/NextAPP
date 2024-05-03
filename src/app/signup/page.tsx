da074255ecf338fd10aea28df5ab1128fdec57af
Parchello <parchellos@gmail.com>
2024-05-03 18:25:15 +0300

    midlware rout add

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import toast from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function SignUpPage() {
   const router = useRouter();
@@ -82,6 +82,7 @@ export default function SignUpPage() {
       <Link className="hover:bg-green-500" href="/login">
         Login Page
       </Link>
+      <Toaster />
     </div>
   );
 }

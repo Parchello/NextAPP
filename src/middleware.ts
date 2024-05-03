f0abe6bcf11b15b69a68e9de9cbb496058a392b2
Parchello <parchellos@gmail.com>
2024-05-03 19:43:11 +0300

    nodelailer start

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isPublicPath = path === "/login" || path === "/signup";
@@ -17,7 +16,6 @@ export function middleware(request: NextRequest) {
   }
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: ["/", "/profile", "/login", "/signup"],
 };

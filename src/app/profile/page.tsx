28fb1e814a5a83580b1679d83bab3ab0c0df26bc
Parchello <parchellos@gmail.com>
2024-05-03 19:09:12 +0300

    profile details

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,10 +3,11 @@ import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-import React from "react";
+import React, { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 export default function ProfilePage() {
+  const [data, setData] = useState("");
   const router = useRouter();
   const logout = async () => {
     try {
@@ -18,16 +19,36 @@ export default function ProfilePage() {
       toast.error("Logout failed");
     }
   };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("api/users/me");
+    console.log(res);
+    setData(res.data.data.username);
+  };
+
   return (
     <div>
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2 className="bg-green-500 ">
+        {data === "" ? (
+          "No data"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <button
         onClick={logout}
         className="bg-blue-500 hover:bg-slate-600 text-white rounded py-2 px-4 font-bold mt-4"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-purple-500 hover:bg-slate-600 text-white rounded py-2 px-4 font-bold mt-4"
+      >
+        Get user details
       </button>
       <Toaster />
     </div>

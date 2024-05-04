f6c9b558bf541331afa1eedff7f83ab3459d770b
Parchello <parchellos@gmail.com>
2024-05-04 11:02:14 +0300

    mailer

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -4,6 +4,33 @@ import bcryptjs from "bcryptjs";
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
+    const hashedToken = bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(
+        userId,
+        { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000 },
+        { new: true, runValidators: true }
+      );
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(
+        userId,
+        {
+          forgotPasswordToken: hashedToken,
+          forgotPasswordTokenExpiry: Date.now() + 3600000,
+        },
+        { new: true, runValidators: true }
+      );
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "3fd364695517df",
+        pass: "7383d58fd399cf",
+      },
+    });
   } catch (error: any) {
     throw new Error(error.message);
   }
